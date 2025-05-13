6c478968f9cdd45246704e70690345cd864fa78c
Dhasmana, Saksham <saksham.dhasmana@united.com>
2025-05-13 22:29:44 +0530

    string variables added

--- hello.go
+++ hello.go
@@ -4,4 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	// string variable
+	var name string
+	name = "John Doe"
+	fmt.Println("Hello, " + name + "!")
+
+	lastname := "Brahmin"
+	fmt.Println("Latname: " + lastname)
+
+	fmt.Printf("Name: %s, Lastname: %s\n", name, lastname)
+	// decimal variable
+	fmt.Printf(("Salary: %f\n"), 1000.23)
+
+	fmt.Printf("Salary: %.2f\n", 1000.238)
+
+	fmt.Printf("Age: %d\n", 25)
+
 }
